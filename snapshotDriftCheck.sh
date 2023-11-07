e66608b595f7d965f4232dccd4dabb18ca324cc3
Andrew Guard <andrew.guard@red-gate.com>
2023-11-07 15:28:00 -0600

    update snapshot syntax

--- snapshotDriftCheck.sh
+++ snapshotDriftCheck.sh
@@ -1,13 +1,14 @@
-## take pre-deployment snapshot of shadow for drift report
-flyway snapshot -url=$(Shadow_JDBC_URL) -user=$(shadow_username) -password=$(shadow_password) -snapshot.filename="C:\snapshots\VCurrent_snapshot"
+## take pre-deployment snapshot of target for drift report
+## NOTE: In a pipeline, this would be a post-release step, and then the artifiact should be saved off somewhere
+flyway snapshot -url=$(target_JDBC_URL) -user=$(target_username) -password=$(target_password) -snapshot.filename="C:\snapshots\VCurrent_snapshot"
 
 ## Pre-Deploy Drift Check - Use snapshots for Drift and Change reports
 flyway info check -drift -dryrun -code  -check.deployedSnapshot="C:\snapshots\VCurrent_snapshot" -check.failOnDrift="$(Boolean)"  -schemas="$(schemas)" -url="$(target_database_JDBC)" -user="$(userName)" -password="$(password)" -check.reportFilename="$(System.ArtifactsDirectory)\$(databaseName)-$(Build.BuildId)-DriftReport.html" -licenseKey=$(FLYWAY_LICENSE_KEY) -workingDirectory="$(WORKING_DIRECTORY)" 
 
 ## if there is drift - generate script to represent drift
-## generate script to run against cleaned shadow
+## generate script to run against cleaned target
 
-## take pre-deployment snapshot of shadow for drift report
+## take pre-deployment snapshot of target for drift report
 flyway snapshot -url=jdbc:oracle:thin:@//localhost:1521/Acceptance -user="HR" -password="Redgate1" -snapshot.filename="C:\snapshots\VCurrent_snapshot"
 
 ## Pre-Deploy Drift Check - Use snapshots for Drift and Change reports
@@ -15,11 +16,11 @@ flyway info check -drift -dryrun -code  -check.deployedSnapshot="C:\snapshots\VC
 
 
 
-## deploy to Shadow
-## flyway migrate -url=$(Shadow_JDBC_URL) -user=$(shadow_username) -password=$(shadow_password)
+## deploy to target
+## flyway migrate -url=$(target_JDBC_URL) -user=$(target_username) -password=$(target_password)
 
-## take snapshot for change report and post deploy check - how does deployment change objects in shadow to estimate changes in target db
-flyway snapshot -url=$(Shadow_JDBC_URL) -user=$(shadow_username) -password=$(shadow_password) -snapshot.filename="C:\snapshots\VNext_snapshot"
+## take snapshot for change report and post deploy check - how does deployment change objects in target to estimate changes in target db
+flyway snapshot -url=$(target_JDBC_URL) -user=$(target_username) -password=$(target_password) -snapshot.filename="C:\snapshots\VNext_snapshot"
 
 ## deploy to Target Database
 ## flyway migrate -url=$(target_JDBC_URL) -user=$(username) -password=$(password)
