dba3a4af67849aa9e2cf8efed5559b0fe3027f92
Andrew Guard <andrew.guard@red-gate.com>
2024-02-08 17:34:55 -0600

    update CSV pipelines to latest Flyway versions with TOML

--- Azure/Azure-CSV-Deployment/PowershellDeploymentDefinition.ps1
+++ Azure/Azure-CSV-Deployment/PowershellDeploymentDefinition.ps1
@@ -1,4 +1,4 @@
-param ($deploymentTargetsList, $baselineVersion, $flywayLicenseKey, $workingDirectory)
+param ($deploymentTargetsList, $baselineVersion, $flywayLicenseKey, $workingDirectory, $schemas)
 
 write-host "deploymentTargetsList: $deploymentTargetsList"
 
@@ -6,7 +6,7 @@ Import-csv -path $deploymentTargetsList |
     Foreach-object {
 
 			$currentJDBC = $_.JDBCString
-            $deployment = "flyway migrate -outOfOrder='true' -baselineOnMigrate='true' -errorOverrides='S0001:0:I'- -baselineVersion=" + $baselineVersion + " -licenseKey=" +  $flywayLicenseKey + " -configFiles=" + $workingDirectory + "\flyway.conf" + " -locations=filesystem:" + $workingDirectory + '\migrations' + " -url='" + $currentJDBC + "'" 
+            $deployment = "flyway migrate -outOfOrder='true' -baselineOnMigrate='true' -errorOverrides='S0001:0:I'- -baselineVersion=" + $baselineVersion + " -schemas=" + $schemas + " -licenseKey=" +  $flywayLicenseKey + " -locations=filesystem:" + $workingDirectory  + "\migrations -url='" + $currentJDBC + "'" 
 			write-host "flywayCommand:" + $deployment
             write-host "deploying to: $currentJDBC"
             Invoke-Expression $deployment
