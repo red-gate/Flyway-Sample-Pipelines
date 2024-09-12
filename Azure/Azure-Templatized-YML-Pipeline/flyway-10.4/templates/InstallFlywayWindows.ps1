3c40b803cc99d58d31b494a160a7fb4210d660d8
Andrew Guard <Andrew.Guard@red-gate.com>
2024-09-12 11:27:07 -0500

    update flyway installation for Windows script

--- Azure/Azure-Templatized-YML-Pipeline/flyway-10.4/templates/InstallFlywayWindows.ps1
+++ Azure/Azure-Templatized-YML-Pipeline/flyway-10.4/templates/InstallFlywayWindows.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 #Flyway Version to Use - Check here for latest version information - https://documentation.red-gate.com/fd/command-line-184127404.html
  
-$flywayVersion = '10.17.0'
+$flywayVersion = '10.17.3'
  
 #Flyway URL to download CLI
  
@@ -15,6 +15,18 @@ $DownloadZipFile = "C:\FlywayCLI\" + $(Split-Path -Path $Url -Leaf)
 #Assign location for Flyway to extracted to
  
 $ExtractPath = "C:\FlywayCLI\"
+
+if (Test-Path $ExtractPath) {
+   
+    Write-Host "Folder Exists"
+    Exit
+}
+else
+{
+    #PowerShell Create directory if not exists
+    New-Item $ExtractPath -ItemType Directory
+    Write-Host "Folder Created successfully"
+}
 
 #SilentlyContinue informs PowerShell to download the CLI without a progress bar. This often drastically improves the download time.
  
@@ -47,52 +59,22 @@ if (Get-Command flyway) {
         
         # Extract the CLI to the desired location
         
-        $ExtractShell = New-Object -ComObject Shell.Application
+        # $ExtractShell = New-Object -ComObject Shell.Application
         
-        $ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items()
+        # $ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items()
         
-        $ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
-        Start-Process $ExtractPath
+        # $ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+        # Start-Process $ExtractPath
+
+        Expand-Archive -Path $DownloadZipFile -DestinationPath $ExtractPath
         
         # Update PATH Variable with Flyway CLI - Azure DevOps #
         
-        echo "##vso[task.setvariable variable=path]$(PATH);C:\FlywayCLI\flyway-$flywayVersion"
+        $Env:Path += ";C:\FlywayCLI\flyway-$flywayVersion"
+        $Env:Path
+
+        "flyway -v" | cmd.exe
         Exit
     }
 
 }
- 
-if (Test-Path $ExtractPath) {
-   
-    Write-Host "Folder Exists"
-    Exit
- 
-}
-else
-{
-  
-    #PowerShell Create directory if not exists
-    New-Item $ExtractPath -ItemType Directory
-    Write-Host "Folder Created successfully"
-}
- 
-# Download the CLI to the desired location
- 
-Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
- 
-# Extract the CLI to the desired location
- 
-$ExtractShell = New-Object -ComObject Shell.Application
- 
-$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items()
- 
-$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
-Start-Process $ExtractPath
- 
-# Update PATH Variable with Flyway CLI - Azure DevOps #
- 
-echo "##vso[task.setvariable variable=path]$(PATH);C:\FlywayCLI\flyway-$flywayVersion"
- 
-# Update PATH variable with Flyway CLI - Generic - Comment above and uncomment below to take affect #
- 
-# [Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";${ExtractPath}flyway-$flywayVersion", [EnvironmentVariableTarget]::Machine)
