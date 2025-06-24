9e2ee7123f6b20f3760776d7a660314ab39c87f8
Andrew Guard <Andrew.Guard@red-gate.com>
2025-06-24 16:11:04 -0500

    not working with filtering but a start

--- Helpers/projectInitCompareOptions.ps1
+++ Helpers/projectInitCompareOptions.ps1
@@ -1,5 +1,3 @@
-# For this to work you will need to run "flyway auth -IAgreeToTheEula", and update project name and Database URL
-
 $databaseType = "SqlServer" # alt values: SqlServer Oracle PostgreSql MySql 
 $Url = "jdbc:sqlserver://localhost;databaseName=NewWorldDB_Dev;encrypt=false;integratedSecurity=true;trustServerCertificate=true"
 $User = ""
@@ -18,6 +16,26 @@ flyway init "-init.projectName=$projectName" "-init.databaseType=$databaseType"
 -replace 'ignoreUsersPermissionsAndRoleMemberships\s*=\s*false', 'ignoreUsersPermissionsAndRoleMemberships = true' `
 -replace 'includeDependencies\s*=\s*true', 'includeDependencies = false' |
 Set-Content -Path "flyway.toml"
+
+# Define the file path
+$filePath = "Filter.scpf"
+
+# Check if the file exists
+if (Test-Path -Path $filePath) {
+    # Load the XML file
+    $xml = [xml](Get-Content -Path $filePath)
+
+    # Find the <None> element and update its child nodes
+    $noneElement = $xml.NamedFilter.Filter.Filters.None
+    $noneElement.Include = "False"
+    $noneElement.Expression = "((@SCHEMA LIKE 'cdc%'))"
+
+    # Save the updated XML back to the file
+    $xml.Save($filePath)
+    Write-Host "Filter.scpf updated successfully."
+} else {
+    Write-Host "Filter.scpf does not exist."
+}
 
 flyway diff model "-diff.source=dev" "-diff.target=schemaModel" "-environments.dev.url=$Url" "-environments.dev.user=$User" "-environments.dev.password=$Password" "-environments.dev.schemas=$Schemas"
 flyway diff generate "-diff.source=schemaModel" "-diff.target=empty" "-generate.types=baseline" "-generate.version=1.0.0" 
