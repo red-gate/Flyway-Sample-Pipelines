68b12c22552cd97f90009895e591cd461581f2f3
Andrew Guard <Andrew.Guard@red-gate.com>
2025-06-26 13:34:15 -0500

    working with filtering

--- Helpers/projectInitCompareOptions.ps1
+++ Helpers/projectInitCompareOptions.ps1
@@ -29,9 +29,7 @@ if (Test-Path -Path $filePath) {
     $noneElement = $xml.NamedFilter.Filter.Filters.None
     $noneElement.Include = "False"
     $noneElement.Expression = "((@SCHEMA LIKE 'cdc%'))"
-    
-    $currentPath = Get-Location
-    $filePath = Join-Path -Path $currentPath -ChildPath $filePath
+
     # Save the updated XML back to the file
     $xml.Save($filePath)
     Write-Host "Filter.scpf updated successfully."
