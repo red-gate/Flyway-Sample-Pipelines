2e454edec21cddd9f6dbb3b4d369c46aae06ed17
Andrew Guard <Andrew.Guard@red-gate.com>
2025-12-08 09:19:00 -0600

    create a new cli file where individual line copy and paste will work. create user defined inputs on the larger codebases to allow review one by one

--- Generic-POC-Flyway-Commands/Unix/cookbook.sh
+++ Generic-POC-Flyway-Commands/Unix/cookbook.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
 # Variables to be changed by user
-CONFIG_FILES="/path/to/flyway.toml,/path/to/flyway.user.toml"
-WORKING_DIRECTORY="/path/to/working/directory"
+CONFIG_FILES="C:\WorkingFolders\FWD\NewWorldDB\flyway.toml,C:\WorkingFolders\FWD\NewWorldDB\flyway.user.toml"
+WORKING_DIRECTORY="C:\WorkingFolders\FWD\NewWorldDB"
 SCHEMA_MODEL_LOCATION="./schema-model"
 ENVIRONMENT="test"
 TARGET="043.20250716213211"
 CHERRY_PICK="045.20251106201536"
 
 # generic deployment
+read -p "Press Enter to run migrate command..."
 flyway migrate -configFiles="$CONFIG_FILES" -workingDirectory="$WORKING_DIRECTORY" -schemaModelLocation="$SCHEMA_MODEL_LOCATION" -schemaModelSchemas= -environment=$ENVIRONMENT
 
+# ================================================================================
+
 # create snapshot after changes
+read -p "Press Enter to run snapshot command..."
 flyway snapshot -environment=$ENVIRONMENT -filename=snapshothistory:current -configFiles="$CONFIG_FILES" -workingDirectory="$WORKING_DIRECTORY" -schemaModelLocation="$SCHEMA_MODEL_LOCATION"
 
+# ================================================================================
+
 # undo back to a specific target number
+read -p "Press Enter to run undo command..."
 flyway undo -configFiles="$CONFIG_FILES" -workingDirectory="$WORKING_DIRECTORY" -schemaModelLocation="$SCHEMA_MODEL_LOCATION" -schemaModelSchemas= -environment=$ENVIRONMENT -target=$TARGET
 
+# ================================================================================
+
 # cherryPick forward
+read -p "Press Enter to run cherry-pick migrate command..."
 flyway migrate -configFiles="$CONFIG_FILES" -workingDirectory="$WORKING_DIRECTORY" -schemaModelLocation="$SCHEMA_MODEL_LOCATION" -schemaModelSchemas= -environment=$ENVIRONMENT -cherryPick=$CHERRY_PICK
+
+# ================================================================================
 
 # drift and code analysis report with snapshots
 
     # run drift and code analysis (TO SEE DRIFT ALTER TARGET DB OUTSIDE OF FLYWAY)
     # check can be configured to fail on drift or code analysis triggering
     # it's possible to capture changes as well, but it is a duplication of what's stored in schema model and requires an extra database to deploy to in a CI fashion
+    read -p "Press Enter to run drift check command..."
     flyway check -drift -code -dryrun -environment=$ENVIRONMENT -check.code.failOnError=false -check.failOnDrift=false -check.deployedSnapshot=snapshothistory:current -configFiles="$CONFIG_FILES" -workingDirectory="$WORKING_DIRECTORY" -schemaModelLocation="$SCHEMA_MODEL_LOCATION"
 
+    # ================================================================================
+
     # generic deployment
+    read -p "Press Enter to run migrate command..."
     flyway migrate -configFiles="$CONFIG_FILES" -workingDirectory="$WORKING_DIRECTORY" -schemaModelLocation="$SCHEMA_MODEL_LOCATION" -schemaModelSchemas= -environment=$ENVIRONMENT
     
+    # ================================================================================
+
     # create snapshot after changes
+    read -p "Press Enter to run snapshot command..."
     flyway snapshot -environment=$ENVIRONMENT -filename=snapshothistory:current -configFiles="$CONFIG_FILES" -workingDirectory="$WORKING_DIRECTORY" -schemaModelLocation="$SCHEMA_MODEL_LOCATION"
